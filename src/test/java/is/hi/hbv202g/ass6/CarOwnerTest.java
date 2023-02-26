407786f3c19fad76aff2cddf7ca549118b8118a8
Adrian Roszkowski <adr11@hi.is>
2023-02-26 18:21:55 +0000

    Created fields for testing

--- src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
@@ -1,16 +1,26 @@
 package is.hi.hbv202g.ass6;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CarOwnerTest {
 
+    public static final String JOHN_DOE = "John Doe";
+    private Car car;
+    private CarOwner carOwner;
+
+    @Before
+    public void initTest(){
+
+        car = new Car("Beetle");
+        carOwner = new CarOwner(JOHN_DOE, car);
+    }
+
     @Test
     public void getName() {
-        Car car=new Car("Beetle");
-        CarOwner carOwner = new CarOwner("John Doe", car);
-        assertEquals("John Doe", carOwner.getName());
+        assertEquals(JOHN_DOE, carOwner.getName());
     }
 
 }
